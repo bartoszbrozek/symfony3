d3f8a64ffefe22a849d619174a3853e5e32e7043
Barteł <bartekbrozek@gmail.com>
2016-10-09 18:15:39 +0200

    KURWA, działa

--- src/AppBundle/Controller/BartekController.php
+++ src/AppBundle/Controller/BartekController.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class BartekController extends Controller {
 
@@ -13,17 +15,46 @@ class BartekController extends Controller {
      */
     public function showAction($name) {
 
+        $pdo = $this->container->get('db1');
+
+        $query = $pdo->prepare("SELECT * FROM note");
+        $query->execute();
+        $query = $query->fetchAll();
+
+        $login = $this->container->getParameter('database_host');
+
         $notes = [
             1 => 'costam',
             2 => 'nie',
             3 => 'tak'
         ];
 
+        var_dump($query);
+
         return $this->render('bartek/show.html.twig', [
                     'name' => $name,
-                    'notes' => $notes
+                    'notes' => $notes,
+                    'login' => $login
                         ]
         );
+    }
+
+    /**
+     * @Route("/bartek/{name}/notes", name="notes")
+     * @Method("GET")
+     */
+    public function getNotesAction() {
+
+        $notes = [
+                ['id' => 1, 'username' => 'Barteł', 'login' => 'dupa'],
+                ['id' => 2, 'username' => 'Marteł', 'login' => 'zupa']
+        ];
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
     }
 
 }
